16ff2626fada1f4d58252b50c78cb4880b940804
ka7299-cyber <ka7299@smps.km.edu.tw>
2026-03-29 12:46:53 +0800

    Initial upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const cacheName = 'v4-vocab-app';
+const assets = [
+  './',
+  './index.html',
+  './words.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
